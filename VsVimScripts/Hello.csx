f4927911513a1be98f3d8aaac673acff519b5da8
Rager Florian <florianrager90@gmail.com>
2019-10-15 21:44:55 +0200

    adds folder for vsvimscripts and example vim script

---
+++ VsVimScripts/Hello.csx
@@ -0,0 +1,3 @@
+using System.Windows;
+
+MessageBox.Show("Hello, World");
--- setup.cmd
+++ setup.cmd
@@ -1,2 +1,5 @@
 set vimrcPath=%CD%\.vimrc
+set vsVimScriptsPath=%CD%\VsVimScripts\
 copy "%vimrcPath%" "%userprofile%"
+mkdir "%userprofile%\VsVimScripts\"
+copy "%vsVimScriptsPath%" "%userprofile%\VsVimScripts"
